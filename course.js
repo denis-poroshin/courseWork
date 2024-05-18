b4c6d9c496280def30d69c509abccfb5d0e9968b
original <git@github.com:denis-poroshin/variables.git>
2024-05-18 16:00:07 +0300

    first commit

---
+++ course.js
@@ -0,0 +1,10 @@
+const application = () => {
+    while (true) {
+        let theCommandLine = prompt("Введите команду");
+        if (theCommandLine === "end") {
+            break
+        }
+    }
+
+}
+application();
